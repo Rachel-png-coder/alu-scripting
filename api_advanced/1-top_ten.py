117cf0313a2b6df75f3ed90dfd3487e5b950744f
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-20 10:28:32 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,47 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+'''
+A module containing functions for working with the Reddit API.
+The module retrieves the titles of the top 10 posts from a given subreddit.
+'''
 
 import requests
 
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.'''
 
 def top_ten(subreddit):
-    """Main function"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/126.0.0.0 Safari/537.36"
+    '''Retrieves the title of the top ten posts from a given subreddit.'''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        hot_posts = response.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in hot_posts]
-    except Exception:
-        print(None)
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+
+    # If the status code is 200, the subreddit exists
+    if res.status_code == 200:
+        try:
+            # Print the titles of the top 10 posts
+            for post in res.json()['data']['children'][0:10]:
+                print(post['data']['title'])
+        except KeyError:
+            print(None)
+    else:
+
+        print("OK") 
