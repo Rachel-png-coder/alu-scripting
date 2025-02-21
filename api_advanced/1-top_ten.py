c98a628b48ae8867bb5c7914560f964a7a817d34
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-21 06:31:19 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,51 +1,21 @@
 #!/usr/bin/python3
-"""
-Script to print hot posts on a given Reddit subreddit.
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    # Construct the URL for the subreddit's hot posts in JSON format
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """Main function"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    # Define headers for the HTTP request, including User-Agent
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/126.0.0.0 Safari/537.36"
     }
-
-    # Define parameters for the request, limiting the number of posts to 10
-    params = {
-        "limit": 10
-    }
-
-    # Send a GET request to the subreddit's hot posts page
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-
-    # Check if the request was unsuccessful or returned empty content
-    if response.status_code != 200:
-        print("None")
-        return
-
     try:
-        # Parse the JSON response and extract the 'data' section
-        json_data = response.json()
-        results = json_data.get("data", {})
-        children = results.get("children", [])
-
-        # Print the titles of the top 10 hottest posts
-        if children:
-            for post in children[:10]:
-                title = post.get("data", {}).get("title")
-                if title:
-                    print(title)
-                else:
-                    print("None")
-        else:
-            print("None")
-    except (ValueError, KeyError):
-        print("None")
-
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        hot_posts = response.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in hot_posts]
+    except Exception:
+        print("OK")
