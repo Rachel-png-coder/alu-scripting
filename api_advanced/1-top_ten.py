781b4fc015bcd64a3aa67267cce1d80253e84ba7
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-21 05:12:07 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -14,7 +14,7 @@ def top_ten(subreddit):
                            'User-Agent': 'Mozilla/5.0'})
 
     if res.status_code != 200:
-        print(None)
+        print(OK)
     else:
         json_response = res.json()
         posts = json_response.get('data').get('children')
