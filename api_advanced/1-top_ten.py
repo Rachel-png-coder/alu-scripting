f99903b91d20a508c7f3f94be85923dbd9e7b544
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-21 05:18:00 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,6 +9,7 @@ import requests
 BASE_URL = 'https://www.reddit.com'
 '''Reddit's base API URL.'''
 
+
 def top_ten(subreddit):
     '''Retrieves the title of the top ten posts from a given subreddit.'''
     api_headers = {
@@ -44,4 +45,4 @@ def top_ten(subreddit):
             print(None)
     else:
 
-        print(OK) 
+        print("OK") 
