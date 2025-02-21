3044e0c28084fee63f99e44561dbf01175302a59
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-21 05:06:51 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,7 +13,10 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts from a subreddit."""
+    """
+    Prints the titles of the first 10 hot posts from a subreddit.
+    """
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0 (compatible; top_ten_script/1.0)"}
 
