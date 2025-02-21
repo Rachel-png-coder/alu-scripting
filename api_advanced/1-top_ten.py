d464f069e326e8bfe657e757ed59f14c2d9ff689
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-21 06:37:46 -0800

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
-"""A module to query the Reddit API for hot posts."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed in a subreddit.
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    Args:
-        subreddit (str): The name of the subreddit to query.
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    Returns:
-        None: Prints "None" if subreddit is invalid or request fails.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-
-    # Send request with no redirects allowed
-    res = requests.get(url, headers=headers, allow_redirects=False)
-
-    # If request fails (invalid subreddit or other error)
-    if res.status_code != 200:
-        print("None")
-        return
-
-    try:
-        # Extract post data from JSON response
-        json_response = res.json()
-        posts = json_response.get("data", {}).get("children", [])
-
-        if not posts:
-            print("None")
-            return
-
-        # Print the titles of the first 10 hot posts
-        for post in posts:
-            print(post.get("data", {}).get("title", "None"))
-
-    except ValueError:
-        print("None")
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
+        print(None)
